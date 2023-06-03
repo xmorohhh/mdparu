0d905456b3f534a83e750d6ebb41400343228566
Sparkymon777 <105107888+Sparkymon777@users.noreply.github.com>
2023-06-03 09:05:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,9 +100,7 @@ async function Xasena() {
         require(__dirname + "/package.json").version
       }\nTotal Plugins : ${events.commands.length}\nWorktype: ${
         config.WORK_TYPE
-      }\nPrefix: ${
-        prefix
-      }\nGit: *https://github.com/Sparkymon777/Aswin-MD*\`\`\``;
+      }\nPrefix: ${prefix}\nGit: *https://github.com/Sparkymon777/Aswin-MD*\`\`\``;
       conn.sendMessage(conn.user.id, { text: str });
 
       try {
